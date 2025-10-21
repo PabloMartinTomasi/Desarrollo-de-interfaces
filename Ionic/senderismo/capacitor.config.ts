2961df028a21456052e9183131cc37ecfc6006f5
PabloMartinTomasi <pablo.martintomasi24@campusfp.es>
2025-10-21 12:16:49 +0200

    ionic en interfaces

---
+++ Ionic/senderismo/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'senderismo',
+  webDir: 'www'
+};
+
+export default config;
